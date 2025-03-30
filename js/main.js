17df291183cf4436953de02895f74844d17d71c9
Сергей Комыза <s.komiza@yandex.ru>
2025-03-30 13:32:11 +0300

    устранил баг с дерганьем слайдера

--- js/main.js
+++ js/main.js
@@ -14,7 +14,14 @@ const sliders = () => {
     const swiperMobile = new Swiper('.js-sliderMobile', {
         loop: true,
         slidesPerView: 3,
-        spaceBetween: 50
+        spaceBetween: 50,
+        // loopAdditionalSlides: 2,/* loopAdditionalSlides добавил для устранения дерганья при переходе с последнего на первый слайд в зацикленном слайдере */
+        // loopPreventsSlide: true,/* loopPreventsSlide добавил для устранения дерганья при переходе с последнего на первый слайд в зацикленном слайдере */
+        autoplay: {
+            delay: 3000,
+            // disableOnInteraction: false /* disableOnInteraction добавил для устранения дерганья при переходе с последнего на первый слайд в зацикленном слайдере */
+        },
+        speed: 1000
     })
 
 }
