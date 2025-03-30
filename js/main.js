f89b031fd8bda75fbde9de85af57db25890a2765
Сергей Комыза <s.komiza@yandex.ru>
2025-03-30 13:05:00 +0300

    поменял фотки

--- js/main.js
+++ js/main.js
@@ -2,20 +2,19 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ СЛАЙДЕР SWIPER (https://swiperjs.com/get-started) 
 const sliders = () => {
-    const swiper = new Swiper('.js-slider', {
-        loop: true,
-        // pagination: {
-        //     el: '.swiper-pagination',
-        // },
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
-    });
+    document.querySelectorAll('.js-slider').forEach((item, i) => {
+        const swiper = new Swiper(item, {
+            loop: false,
+            autoplay: {
+                delay: 1000 + ((i+1)*1000)
+            }
+        });
+
+    })
     const swiperMobile = new Swiper('.js-sliderMobile', {
         loop: true,
         slidesPerView: 3,
-        spaceBetween: 40
+        spaceBetween: 50
     })
 
 }
